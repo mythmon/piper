65eaae560e12178942f5fb4db32d1fd6ddd26ca8
Mike Cooper <mythmon@gmail.com>
2013-07-06 12:13:44 -0700

    API for creating transactions.

--- piper/api.py
+++ piper/api.py
@@ -1,45 +1,33 @@
 import json
 
-from flask import Blueprint, jsonify, Response
+from flask import Blueprint, jsonify, Response, request
 
 
 blueprint = Blueprint('api', __name__, url_prefix='/api')
 
 
-mock_transactions = [
-    {
-        'id': 0,
-        'date': '1372896000000',
-        'name': 'Fourth of July Snacks!',
-        'category': 'Food',
-        'amount': 12.34,
-    }, {
-        'id': 1,
-        'date': '1372809600000',
-        'name': 'Cute outfit',
-        'category': 'Clothes',
-        'amount': 20.01,
-    }, {
-        'id': 2,
-        'date': '1372550400000',
-        'name': 'Sandals',
-        'category': 'Clothes',
-        'amount': 48.25,
-    }, {
-        'id': 2,
-        'date': '1372464000000',
-        'name': 'Awesome app',
-        'category': 'Software',
-        'amount': 0.99,
-    }
-]
+transactions = []
 
 
 @blueprint.route('/transaction', methods=['GET'])
 def transaction_list():
-    return Response(json.dumps(mock_transactions), mimetype='application/json')
+    return Response(json.dumps(transactions), mimetype='application/json')
 
 
 @blueprint.route('/transaction/<int:id>', methods=['GET'])
 def transaction_detail(id):
-    return jsonify(mock_transactions[id])
+    return jsonify(transactions[id])
+
+
+@blueprint.route('/transaction', methods=['POST'])
+def transaction_create():
+    data = request.get_json()
+    trans = {
+        'date': data['date'],
+        'name': data['name'],
+        'category': data['category'],
+        'amount': data['amount'],
+        'id': len(transactions),
+    }
+    print 'Adding', trans
+    transactions.append(trans)
